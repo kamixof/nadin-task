c8d7c23fe33f40999ee0453ae9337245f4b711d2
kamixof <121563821+kamixof@users.noreply.github.com>
2025-08-08 18:30:40 +0330

    Update ip-tables.sh  add ssh

--- ip-tables.sh
+++ ip-tables.sh
@@ -19,18 +19,21 @@ iptables -A INPUT -i lo -j ACCEPT
 # Allow established and related connections
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# Allow SSH (port 22) from internal IP range (192.168.0.0/16)
+iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 22 -j ACCEPT
+
 # Allow Docker internal network (default bridge network, e.g., 172.17.0.0/16)
 iptables -A FORWARD -s 172.17.0.0/16 -d 172.17.0.0/16 -j ACCEPT
 iptables -A FORWARD -i docker0 -j ACCEPT
 
-# Allow internal IP range (192.168.83.0/24) to access specific ports in DOCKER-USER
+# Allow internal IP range (192.168.0.0/16) to access specific ports in DOCKER-USER
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 80 -j ACCEPT
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 2222 -j ACCEPT
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 9090 -j ACCEPT
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 9100 -j ACCEPT
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 3000 -j ACCEPT
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 9093 -j ACCEPT
-iptables -A DOCKER-USER -s 1192.168.0.0/16 -p tcp --dport 5000 -j ACCEPT
+iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 5000 -j ACCEPT
 iptables -A DOCKER-USER -s 192.168.0.0/16 -p tcp --dport 8001 -j ACCEPT
 
 # Drop all other external traffic in DOCKER-USER
@@ -45,4 +48,3 @@ iptables-save > /etc/iptables/rules.v4
 # Display current iptables rules
 echo "Current iptables rules:"
 iptables -L -v -n
-
